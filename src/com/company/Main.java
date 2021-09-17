c572409def41c08223e092332f7bcbc7decadf65
Daylan Jeffries <DJ211285@reigate.ac.uk>
2021-09-17 15:27:47 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //hi
+        //  System.out.println("Hello world");
+        // System.out.print("Hello world again!");
+
+      //  Scanner input = new Scanner(System.in);
+
+    //    System.out.println("Enter your name");
+       // String personname = input.next();
+       // System.out.println(personname);
+     //   System.out.println("Enter your surname");
+      //  String surname = input.next();
+      //  System.out.println("Hi " + personname + " " + surname);
+
+    for (int i = 0; i <5; i++) {
+        System.out.println("Hi there");
+    }
+
+    int b = 0;
+    while (b<10) {
+        System.out.println(2 * b);
+        b++;
+    }
+
+    int c = 27;
+    if(c==27){
+        System.out.println("Boo!");
+    }
+    }
+}
